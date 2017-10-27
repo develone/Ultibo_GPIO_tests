45e5f706a6614737440014c0607d9d0ff293ff9d
Edward Vidal Jr <develone@sbcglobal.net>
2017-10-27 15:54:42 +0000

    Testing prb code on Rpi, Rpi2B, and Rpi3B with -O2 & -O4
    Rpi w/ -O2 3.9 MHz
    Jitter count for 0 us: 295502293
    Jitter count for 1 us: 85796301
    Jitter count for 2 us: 5222195
    Jitter count for 3 us: 1253103
    
    Rpi w/ -O4 4.14 MHz
    Jitter count for 0 us: 320922247
    Jitter count for 1 us: 86592290
    Jitter count for 2 us: 6032045
    Jitter count for 3 us: 447873
    
    Rpi2 w/ -O2 13,8 MHz
    Jitter count for 0 us: 1284615358
    Jitter count for 1 us: 100000000
    
    Rpi2 w/ -O4 15.25 MHz
    Jitter count for 0 us: 1425423702
    Jitter count for 1 us: 100000000
    
    Rpi3 w/ -O2 19.03 MHz
    Jitter count for 0 us: 180326533
    Jitter count for 1 us: 99999994
    Jitter count for 2 us: 1
    Jitter count for 4 us: 1
    
    Rpi3 w/ -O4 21.07 MHz
    Jitter count for 0 us: 2007186625
    Jitter count for 1 us: 99999989
    Jitter count for 2 us: 4
    Jitter count for 3 us: 1

---
+++ RPi2/GPIOPerformanceTest.pas
@@ -0,0 +1,73 @@
+program GPIOPerformanceTest;
+
+{$mode objfpc}{$H+}
+
+uses
+  GlobalConst,
+  GlobalTypes,
+  GlobalConfig,
+  Platform,
+  Threads,
+  Console,
+  Framebuffer,
+  BCM2836,
+  BCM2709,
+  SysUtils,
+  GPIO;
+
+
+const
+  MAX_JITTER = 100000; // us
+  RUN_TIME = 100000000; // us
+
+var
+ WindowHandle:TWindowHandle;
+ startTime, processingTime, prevProcessingTime, jitter, counter : int64;
+ jitterHistogram : array[ 0..MAX_JITTER ] of integer;
+ i : integer;
+
+begin
+  for i := 0 to MAX_JITTER do
+  begin
+    jitterHistogram[ i ] := 0;
+  end;
+  counter := 0;
+  WindowHandle:=ConsoleWindowCreate(ConsoleDeviceGetDefault,CONSOLE_POSITION_FULL,True);
+
+  ConsoleWindowWriteLn(WindowHandle,'GPIO performance test');
+  ConsoleWindowWriteLn(WindowHandle,'Clock cycles per millisecond: ' + inttostr(  CLOCK_CYCLES_PER_MILLISECOND  ) );
+
+  GPIOFunctionSelect(GPIO_PIN_16,GPIO_FUNCTION_OUT);
+  GPIOOutputSet(GPIO_PIN_16,GPIO_LEVEL_LOW);
+
+  DisableIRQ;
+
+  startTime := ClockGetTotal();
+  prevProcessingTime := ClockGetTotal();
+  while ( prevProcessingTime - startTime < RUN_TIME ) do
+  begin
+    PLongWord(BCM2836_GPIO_REGS_BASE + BCM2836_GPSET0)^:=$00010000;
+    processingTime := ClockGetTotal();
+    jitter := processingTime - prevProcessingTime;
+    if ( jitter > MAX_JITTER ) then jitter := MAX_JITTER;
+    PLongWord(BCM2836_GPIO_REGS_BASE + BCM2836_GPCLR0)^:=$00010000;
+    inc( jitterHistogram[ jitter ] );
+    prevProcessingTime := processingTime;
+    inc( counter );
+  end;
+
+  EnableIRQ;
+
+  ConsoleWindowWriteLn(WindowHandle, 'Average toggle rate: ' + floattostr( counter / RUN_TIME ) + ' MHz' );
+
+  for i := 0 to MAX_JITTER do
+  begin
+    if jitterHistogram[ i ] > 0 then
+    begin
+      ConsoleWindowWriteLn(WindowHandle,'Jitter count for ' + inttostr( i ) + ' us: ' +  inttostr( jitterHistogram[ i ] ) );
+    end;
+  end;
+
+  ThreadHalt(0);
+end.
+
